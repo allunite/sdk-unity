e28e2131d35a33151d591ad5746f356a74996ec1
Tuccro <tuccro@yahoo.com>
2017-12-04 01:23:38 +0200

    added Android demo

---
+++ Demo/Android/Assets/AllUniteSdkDemo.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AllUniteSdkDemo : MonoBehaviour {
+
+    public void onStart()
+    {
+		
+    }
+
+    public void requestPermission() {
+    	UniAndroidPermission.RequestPremission (AndroidPermission.ACCESS_COARSE_LOCATION, () => {
+    		// add permit action
+		}, () => {
+    		// add not permit action
+		});
+    }
+
+	public void init() 
+	{
+		AndroidJavaObject allUniteSdk = new AndroidJavaObject("com.allunite.sdk.AllUniteSdk");
+		AndroidJavaObject currentActivity = getCurrentActivity();
+
+		allUniteSdk.CallStatic("init", currentActivity, "Ardas test", "287708C2BE7048A3B4D8518D84E642B3");
+	}
+
+	public void bindDevice() 
+	{
+		AndroidJavaObject allUniteSdk = new AndroidJavaObject("com.allunite.sdk.AllUniteSdk");
+		AndroidJavaObject currentActivity = getCurrentActivity();
+
+		allUniteSdk.CallStatic ("bindDevice", currentActivity, "allunite://main");
+	}
+
+	public void bindFBProfile() 
+	{
+		AndroidJavaObject allUniteSdk = new AndroidJavaObject("com.allunite.sdk.AllUniteSdk");
+		AndroidJavaObject currentActivity = getCurrentActivity();
+
+		allUniteSdk.CallStatic (
+			"bindFBProfile", 
+			"{\"id\": \"12345678\", \"birthday\": \"1/1/1950\", \"first_name\": \"Chris\", \"gender\": \"male\", \"last_name\": \"Colm\", \"link\": \"http://www.facebook.com/12345678\", \"location\": { \"id\": \"110843418940484\", \"name\": \"Seattle, Washington\" }, \"locale\": \"en_US\", \"name\": \"Chris Colm\", \"timezone\": -8, \"updated_time\": \"2010-01-01T16:40:43+0000\", \"verified\": true}", 
+			"whateverFacebookTokenExample");
+	}
+
+	public void tracking()
+	{
+		AndroidJavaObject allUniteSdk = new AndroidJavaObject("com.allunite.sdk.AllUniteSdk");
+		AndroidJavaObject currentActivity = getCurrentActivity();
+
+		currentActivity.Call ("runOnUiThread", new AndroidJavaRunnable (() => 
+			{
+				allUniteSdk.CallStatic ("track", currentActivity, "test", "id");
+			}));
+
+	}
+
+    private AndroidJavaObject getCurrentActivity()
+    {
+        AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        return jc.GetStatic<AndroidJavaObject>("currentActivity");
+    }
+	
+}
---
+++ Demo/Android/Assets/BehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		AndroidJavaObject allUniteSdk = new AndroidJavaObject("com.allunite.sdk.service.BTService");
+		AndroidJavaObject currentActivity = getCurrentActivity();
+
+		allUniteSdk.CallStatic ("startService", currentActivity);
+	}
+
+	private AndroidJavaObject getCurrentActivity()
+	{
+		AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+		return jc.GetStatic<AndroidJavaObject>("currentActivity");
+	}
+}
